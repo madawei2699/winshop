da43a8bf33ffd322b34d4d896e88139023f794ea
yuwenhui <yuwenhui1986@163.com>
2014-03-04 18:40:10 +0800

    add validator

--- Public/js/main.js
+++ Public/js/main.js
@@ -2,15 +2,17 @@ require.config({
 	paths: {
 		'jquery': 'lib/jquery/jquery',
 		'bootstrap': 'lib/bootstrap/bootstrap',
+		'validator': 'lib/bootstrap/bootstrapValidator',
 		'core': 'app/core.js'
 	},
 
 	shim: {
-		'bootstrap': {deps: ['jquery']}
+		'bootstrap': {deps: ['jquery']},
+		'validator': {deps: ['jquery']}
 	}
 });
 
-define(['jquery', 'bootstrap'], function($) {
+define(['jquery', 'bootstrap', 'validator'], function($) {
 
 	$(document)
 		.ajaxStart(function(){
@@ -36,6 +38,74 @@ define(['jquery', 'bootstrap'], function($) {
 		}
 	});
 
+	$('#login-form').bootstrapValidator({
+		fields: {
+			email: {
+				validators: {
+					notEmpty: {
+						message: '您还没有填写邮箱'
+					},
+					emailAddress: {
+						message: '邮箱格式不对哦'
+					}
+				}
+			},
+			password: {
+				validators: {
+					notEmpty: {
+						message: '您还没有填写密码'
+					}
+				}
+			}
+		}
+	});
+
+	$('#register-form').bootstrapValidator({
+		fields: {
+			email: {
+				validators: {
+					notEmpty: {
+						message: '您还没有填写邮箱'
+					},
+					emailAddress: {
+						message: '邮箱格式不对哦'
+					}
+				}
+			},
+			password: {
+				validators: {
+					notEmpty: {
+						message: '您还没有填写密码'
+					},
+					stringLength: {
+						min: 6,
+						max: 30,
+						message: '密码长度为6到30个字符'
+					},
+					identical: {
+						field: 'repassword',
+						message: '两次密码不一致哦'
+					}
+				}
+			},
+			repassword: {
+				validators: {
+					notEmpty: {
+						message: '您还没有输入验证密码'
+					},
+					stringLength: {
+						min: 6,
+						max: 30,
+						message: '密码长度为6到30个字符'
+					},
+					identical: {
+						field: 'password',
+						message: '两次密码输入不一致'
+					}
+				}
+			}
+		}
+	});
 
 	return $.noConflict(true);
 });
