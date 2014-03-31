18f9d423643261f6d3c95240f93882977f00ab33
yuwenhui <yuwenhui1986@163.com>
2014-03-31 21:38:24 +0800

    modify sql

--- winshop.sql
+++ winshop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.4
+-- version 3.4.10
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2014 年 03 月 12 日 08:27
--- 服务器版本: 5.5.32
--- PHP 版本: 5.4.19
+-- 生成日期: 2014 年 03 月 31 日 15:37
+-- 服务器版本: 5.5.34
+-- PHP 版本: 5.4.21
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `winshop_action_log` (
   KEY `action_ip_ix` (`action_ip`),
   KEY `action_id_ix` (`action_id`),
   KEY `user_id_ix` (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='行为日志表' AUTO_INCREMENT=18 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='行为日志表' AUTO_INCREMENT=30 ;
 
 --
 -- 转存表中的数据 `winshop_action_log`
@@ -99,7 +99,19 @@ INSERT INTO `winshop_action_log` (`id`, `action_id`, `user_id`, `action_ip`, `mo
 (14, 10, 1, 2130706433, 'Menu', 123, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1394607243),
 (15, 10, 1, 2130706433, 'Menu', 124, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1394607289),
 (16, 10, 1, 2130706433, 'Menu', 124, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1394607310),
-(17, 10, 1, 2130706433, 'Menu', 123, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1394607316);
+(17, 10, 1, 2130706433, 'Menu', 123, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1394607316),
+(18, 10, 1, 2130706433, 'Menu', 125, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1396141579),
+(19, 10, 1, 2130706433, 'Menu', 125, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1396141592),
+(20, 10, 1, 2130706433, 'Menu', 126, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1396141726),
+(21, 10, 1, 2130706433, 'Menu', 127, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1396141828),
+(22, 10, 1, 2130706433, 'Menu', 128, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1396141868),
+(23, 10, 1, 2130706433, 'Menu', 126, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1396143532),
+(24, 10, 1, 2130706433, 'Menu', 127, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1396143542),
+(25, 10, 1, 2130706433, 'Menu', 128, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1396143552),
+(26, 10, 1, 2130706433, 'Menu', 125, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1396143726),
+(27, 10, 1, 2130706433, 'Menu', 129, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1396144718),
+(28, 10, 1, 2130706433, 'Menu', 130, '操作url：/winshop/index.php?s=/Admin/Menu/add.html', 1, 1396144779),
+(29, 10, 1, 2130706433, 'Menu', 132, '操作url：/winshop/index.php?s=/Admin/Menu/edit.html', 1, 1396144848);
 
 -- --------------------------------------------------------
 
@@ -119,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `winshop_addons` (
   `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '安装时间',
   `has_adminlist` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否有后台列表',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='插件表' AUTO_INCREMENT=16 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='插件表' AUTO_INCREMENT=17 ;
 
 --
 -- 转存表中的数据 `winshop_addons`
@@ -279,7 +291,7 @@ CREATE TABLE IF NOT EXISTS `winshop_auth_group` (
 --
 
 INSERT INTO `winshop_auth_group` (`id`, `module`, `type`, `title`, `description`, `status`, `rules`) VALUES
-(1, 'admin', 1, '默认用户组', '', 1, '1,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,79,80,81,82,83,84,86,87,88,89,90,91,92,93,94,95,96,97,100,102,103,105,106'),
+(1, 'admin', 1, '默认用户组', '', 1, '1,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,79,80,81,82,83,84,86,87,88,89,90,91,92,93,94,95,100,102,103,217,218,219,220,221,222,223'),
 (2, 'admin', 1, '测试用户', '测试用户', 1, '1,2,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,79,80,82,83,84,88,89,90,91,92,93,96,97,100,102,103,195');
 
 -- --------------------------------------------------------
@@ -312,7 +324,7 @@ CREATE TABLE IF NOT EXISTS `winshop_auth_rule` (
   `condition` varchar(300) NOT NULL DEFAULT '' COMMENT '规则附加条件',
   PRIMARY KEY (`id`),
   KEY `module` (`module`,`status`,`type`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=217 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=224 ;
 
 --
 -- 转存表中的数据 `winshop_auth_rule`
@@ -531,7 +543,14 @@ INSERT INTO `winshop_auth_rule` (`id`, `module`, `type`, `name`, `title`, `statu
 (213, 'admin', 1, 'Admin/Menu/sort', '排序', 1, ''),
 (214, 'admin', 1, 'Admin/Channel/sort', '排序', 1, ''),
 (215, 'admin', 1, 'Admin/Category/operate/type/move', '移动', 1, ''),
-(216, 'admin', 1, 'Admin/Category/operate/type/merge', '合并', 1, '');
+(216, 'admin', 1, 'Admin/Category/operate/type/merge', '合并', 1, ''),
+(217, 'admin', 1, 'Admin/Index/index', '基本信息', 1, ''),
+(218, 'admin', 1, 'Admin/Item/index', '商品列表', 1, ''),
+(219, 'admin', 1, 'Admin/Item/draftbox', '草稿箱', 1, ''),
+(220, 'admin', 1, 'Admin/Item/recyle', '回收站', 1, ''),
+(221, 'admin', 1, 'Admin/Index/cleancache', '缓存清理', 1, ''),
+(222, 'admin', 2, 'Admin/Item/index', '商品', 1, ''),
+(223, 'admin', 1, 'Admin/Index/adminlog', '管理日志', 1, '');
 
 -- --------------------------------------------------------
 
@@ -699,14 +718,15 @@ CREATE TABLE IF NOT EXISTS `winshop_document` (
   PRIMARY KEY (`id`),
   KEY `idx_category_status` (`category_id`,`status`),
   KEY `idx_status_type_pid` (`status`,`uid`,`pid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='文档模型基础表' AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='文档模型基础表' AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `winshop_document`
 --
 
 INSERT INTO `winshop_document` (`id`, `uid`, `name`, `title`, `category_id`, `description`, `root`, `pid`, `model_id`, `type`, `position`, `link_id`, `cover_id`, `display`, `deadline`, `attach`, `view`, `comment`, `extend`, `level`, `create_time`, `update_time`, `status`) VALUES
-(1, 1, '', 'OneThink1.0正式版发布', 2, '大家期待的OneThink正式版发布', 0, 0, 2, 2, 0, 0, 0, 1, 0, 0, 30, 0, 0, 0, 1387260660, 1387263112, 1);
+(1, 1, '', 'OneThink1.0正式版发布', 2, '大家期待的OneThink正式版发布', 0, 0, 2, 2, 0, 0, 0, 1, 0, 0, 30, 0, 0, 0, 1387260660, 1387263112, 1),
+(2, 1, '', '测试文章', 2, '', 0, 0, 2, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1396146435, 1396146435, 1);
 
 -- --------------------------------------------------------
 
@@ -728,7 +748,8 @@ CREATE TABLE IF NOT EXISTS `winshop_document_article` (
 --
 
 INSERT INTO `winshop_document_article` (`id`, `parse`, `content`, `template`, `bookmark`) VALUES
-(1, 0, '<h1>\r\n	OneThink1.0正式版发布&nbsp;\r\n</h1>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	<strong>OneThink是一个开源的内容管理框架，基于最新的ThinkPHP3.2版本开发，提供更方便、更安全的WEB应用开发体验，采用了全新的架构设计和命名空间机制，融合了模块化、驱动化和插件化的设计理念于一体，开启了国内WEB应用傻瓜式开发的新潮流。&nbsp;</strong> \r\n</p>\r\n<h2>\r\n	主要特性：\r\n</h2>\r\n<p>\r\n	1. 基于ThinkPHP最新3.2版本。\r\n</p>\r\n<p>\r\n	2. 模块化：全新的架构和模块化的开发机制，便于灵活扩展和二次开发。&nbsp;\r\n</p>\r\n<p>\r\n	3. 文档模型/分类体系：通过和文档模型绑定，以及不同的文档类型，不同分类可以实现差异化的功能，轻松实现诸如资讯、下载、讨论和图片等功能。\r\n</p>\r\n<p>\r\n	4. 开源免费：OneThink遵循Apache2开源协议,免费提供使用。&nbsp;\r\n</p>\r\n<p>\r\n	5. 用户行为：支持自定义用户行为，可以对单个用户或者群体用户的行为进行记录及分享，为您的运营决策提供有效参考数据。\r\n</p>\r\n<p>\r\n	6. 云端部署：通过驱动的方式可以轻松支持平台的部署，让您的网站无缝迁移，内置已经支持SAE和BAE3.0。\r\n</p>\r\n<p>\r\n	7. 云服务支持：即将启动支持云存储、云安全、云过滤和云统计等服务，更多贴心的服务让您的网站更安心。\r\n</p>\r\n<p>\r\n	8. 安全稳健：提供稳健的安全策略，包括备份恢复、容错、防止恶意攻击登录，网页防篡改等多项安全管理功能，保证系统安全，可靠、稳定的运行。&nbsp;\r\n</p>\r\n<p>\r\n	9. 应用仓库：官方应用仓库拥有大量来自第三方插件和应用模块、模板主题，有众多来自开源社区的贡献，让您的网站“One”美无缺。&nbsp;\r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	<strong>&nbsp;OneThink集成了一个完善的后台管理体系和前台模板标签系统，让你轻松管理数据和进行前台网站的标签式开发。&nbsp;</strong> \r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<h2>\r\n	后台主要功能：\r\n</h2>\r\n<p>\r\n	1. 用户Passport系统\r\n</p>\r\n<p>\r\n	2. 配置管理系统&nbsp;\r\n</p>\r\n<p>\r\n	3. 权限控制系统\r\n</p>\r\n<p>\r\n	4. 后台建模系统&nbsp;\r\n</p>\r\n<p>\r\n	5. 多级分类系统&nbsp;\r\n</p>\r\n<p>\r\n	6. 用户行为系统&nbsp;\r\n</p>\r\n<p>\r\n	7. 钩子和插件系统\r\n</p>\r\n<p>\r\n	8. 系统日志系统&nbsp;\r\n</p>\r\n<p>\r\n	9. 数据备份和还原\r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	&nbsp;[ 官方下载：&nbsp;<a href="http://www.onethink.cn/download.html" target="_blank">http://www.onethink.cn/download.html</a>&nbsp;&nbsp;开发手册：<a href="http://document.onethink.cn/" target="_blank">http://document.onethink.cn/</a>&nbsp;]&nbsp;\r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	<strong>OneThink开发团队 2013</strong> \r\n</p>', '', 0);
+(1, 0, '<h1>\r\n	OneThink1.0正式版发布&nbsp;\r\n</h1>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	<strong>OneThink是一个开源的内容管理框架，基于最新的ThinkPHP3.2版本开发，提供更方便、更安全的WEB应用开发体验，采用了全新的架构设计和命名空间机制，融合了模块化、驱动化和插件化的设计理念于一体，开启了国内WEB应用傻瓜式开发的新潮流。&nbsp;</strong> \r\n</p>\r\n<h2>\r\n	主要特性：\r\n</h2>\r\n<p>\r\n	1. 基于ThinkPHP最新3.2版本。\r\n</p>\r\n<p>\r\n	2. 模块化：全新的架构和模块化的开发机制，便于灵活扩展和二次开发。&nbsp;\r\n</p>\r\n<p>\r\n	3. 文档模型/分类体系：通过和文档模型绑定，以及不同的文档类型，不同分类可以实现差异化的功能，轻松实现诸如资讯、下载、讨论和图片等功能。\r\n</p>\r\n<p>\r\n	4. 开源免费：OneThink遵循Apache2开源协议,免费提供使用。&nbsp;\r\n</p>\r\n<p>\r\n	5. 用户行为：支持自定义用户行为，可以对单个用户或者群体用户的行为进行记录及分享，为您的运营决策提供有效参考数据。\r\n</p>\r\n<p>\r\n	6. 云端部署：通过驱动的方式可以轻松支持平台的部署，让您的网站无缝迁移，内置已经支持SAE和BAE3.0。\r\n</p>\r\n<p>\r\n	7. 云服务支持：即将启动支持云存储、云安全、云过滤和云统计等服务，更多贴心的服务让您的网站更安心。\r\n</p>\r\n<p>\r\n	8. 安全稳健：提供稳健的安全策略，包括备份恢复、容错、防止恶意攻击登录，网页防篡改等多项安全管理功能，保证系统安全，可靠、稳定的运行。&nbsp;\r\n</p>\r\n<p>\r\n	9. 应用仓库：官方应用仓库拥有大量来自第三方插件和应用模块、模板主题，有众多来自开源社区的贡献，让您的网站“One”美无缺。&nbsp;\r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	<strong>&nbsp;OneThink集成了一个完善的后台管理体系和前台模板标签系统，让你轻松管理数据和进行前台网站的标签式开发。&nbsp;</strong> \r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<h2>\r\n	后台主要功能：\r\n</h2>\r\n<p>\r\n	1. 用户Passport系统\r\n</p>\r\n<p>\r\n	2. 配置管理系统&nbsp;\r\n</p>\r\n<p>\r\n	3. 权限控制系统\r\n</p>\r\n<p>\r\n	4. 后台建模系统&nbsp;\r\n</p>\r\n<p>\r\n	5. 多级分类系统&nbsp;\r\n</p>\r\n<p>\r\n	6. 用户行为系统&nbsp;\r\n</p>\r\n<p>\r\n	7. 钩子和插件系统\r\n</p>\r\n<p>\r\n	8. 系统日志系统&nbsp;\r\n</p>\r\n<p>\r\n	9. 数据备份和还原\r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	&nbsp;[ 官方下载：&nbsp;<a href="http://www.onethink.cn/download.html" target="_blank">http://www.onethink.cn/download.html</a>&nbsp;&nbsp;开发手册：<a href="http://document.onethink.cn/" target="_blank">http://document.onethink.cn/</a>&nbsp;]&nbsp;\r\n</p>\r\n<p>\r\n	<br />\r\n</p>\r\n<p>\r\n	<strong>OneThink开发团队 2013</strong> \r\n</p>', '', 0),
+(2, 0, '<p>测试法术打击飞发送到范德萨发送到方式方式发生地方都是</p>', '', 0);
 
 -- --------------------------------------------------------
 
@@ -806,6 +827,38 @@ INSERT INTO `winshop_hooks` (`id`, `name`, `description`, `type`, `update_time`,
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `winshop_item`
+--
+
+CREATE TABLE IF NOT EXISTS `winshop_item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL COMMENT '商品名称',
+  `intro` text COMMENT '商品详情',
+  `number` int(5) NOT NULL DEFAULT '0' COMMENT '库存量',
+  `price` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '商品价格',
+  `transport` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '运费',
+  `keyword` varchar(255) NOT NULL DEFAULT '' COMMENT '关键字',
+  `is_del` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0为已删除',
+  `is_new` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为新品',
+  `is_hot` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为热销',
+  `is_promote` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为特价',
+  `is_on_sale` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0为下架，1为上架',
+  `is_set_image` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否已设置图片',
+  `is_lock` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否锁定，1为锁定',
+  `thumb` varchar(255) NOT NULL DEFAULT '' COMMENT '缩略图',
+  `viewnum` int(10) NOT NULL DEFAULT '0' COMMENT '关注度',
+  `favnum` int(10) NOT NULL DEFAULT '0' COMMENT '被收藏次数',
+  `order` tinyint(1) NOT NULL DEFAULT '0' COMMENT '排序',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
+  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最后修改时间',
+  PRIMARY KEY (`id`),
+  KEY `viewnum` (`viewnum`),
+  KEY `update_time` (`update_time`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `winshop_member`
 --
 
@@ -851,7 +904,7 @@ CREATE TABLE IF NOT EXISTS `winshop_menu` (
   `is_dev` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否仅开发者模式可见',
   PRIMARY KEY (`id`),
   KEY `pid` (`pid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=125 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=133 ;
 
 --
 -- 转存表中的数据 `winshop_menu`
@@ -971,7 +1024,15 @@ INSERT INTO `winshop_menu` (`id`, `title`, `pid`, `sort`, `url`, `hide`, `tip`, 
 (121, '排序', 76, 0, 'Channel/sort', 1, '', '', 0),
 (122, '缓存清理', 1, 1, 'Index/cleancache', 0, '清空系统缓存', '常用管理', 0),
 (123, '管理日志', 1, 2, 'Index/adminlog', 0, '', '常用管理', 0),
-(124, '基本信息', 1, 0, 'Index/index', 0, '', '常用管理', 0);
+(124, '基本信息', 1, 0, 'Index/index', 0, '', '常用管理', 0),
+(125, '商品', 0, 1, 'Item/index', 0, '', '', 0),
+(126, '商品列表', 125, 0, 'Item/index', 0, '', '商品管理', 0),
+(127, '草稿箱', 125, 0, 'Item/draftbox', 0, '', '商品管理', 0),
+(128, '回收站', 125, 0, 'Item/recyle', 0, '', '商品管理', 0),
+(129, '新增', 126, 0, 'Item/add', 0, '', '', 0),
+(130, '上架', 126, 0, 'Item/sale', 0, '', '', 0),
+(131, '编辑', 126, 0, 'Item/edit\r', 0, '', '', 0),
+(132, '删除', 126, 0, 'Item/del', 0, '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -1027,7 +1088,14 @@ CREATE TABLE IF NOT EXISTS `winshop_picture` (
   `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
   `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `winshop_picture`
+--
+
+INSERT INTO `winshop_picture` (`id`, `path`, `url`, `md5`, `sha1`, `status`, `create_time`) VALUES
+(1, '/Uploads/Picture/2014-03-30/53377d2cccd5f.png', '', 'f4a5a6ae9aa965ed10971d57e3dbc9ba', 'af0eb640a8c57975c3d2daca6aa88ad9745bb9a3', 1, 1396145452);
 
 -- --------------------------------------------------------
 
@@ -1136,34 +1204,6 @@ CREATE TABLE IF NOT EXISTS `winshop_userdata` (
   UNIQUE KEY `uid` (`uid`,`type`,`target_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-
-
---
--- 表的结构 `winshop_item`
---
-
-CREATE TABLE IF NOT EXISTS `winshop_item` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL COMMENT '商品名称',
-  `intro` text COMMENT '商品详情',
-  `number` int(5) NOT NULL DEFAULT '0' COMMENT '库存量',
-  `price` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '商品价格',
-  `transport` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '运费',
-  `keyword` varchar(255) NOT NULL DEFAULT '' COMMENT '关键字',
-  `is_del` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0为已删除',
-  `is_new` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为新品',
-  `is_hot` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为热销',
-  `is_promote` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1为特价',
-  `is_on_sale` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0为下架，1为上架',
-  `is_set_image` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否已设置图片',
-  `is_lock` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否锁定，1为锁定',
-  `thumb` varchar(255) NOT NULL DEFAULT '' COMMENT '缩略图',
-  `viewnum` int(10) NOT NULL DEFAULT '0' COMMENT '关注度',
-  `favnum` int(10) NOT NULL DEFAULT '0' COMMENT '被收藏次数',
-  `order` tinyint(1) NOT NULL DEFAULT '0' COMMENT '排序',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
-  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最后修改时间',
-  PRIMARY KEY (`id`),
-  KEY `viewnum` (`viewnum`),
-  KEY `update_time` (`update_time`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
